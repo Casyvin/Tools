f285089f5a0fbfdb8ead61cd4dbf799e04633850
Casyvin <379172456@qq.com>
2025-12-23 17:27:36 +0800

    打包22

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,17 +2,10 @@ module com.sws4cloud.pltools {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+    requires javafx.swing;
+    requires javafx.media;
+    requires org.apache.poi.poi;
 
     opens com.sws4cloud.pltools to javafx.fxml;
-    requires org.apache.poi.poi;
-    requires org.apache.poi.ooxml;
-
     exports com.sws4cloud.pltools;
 }
